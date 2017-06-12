7f1f10efa57d3c1dbc5d9a258330b7f43d8cfa45
Marion Craipeau <marion_craipeau@hotmail.fr>
2017-06-12 18:36:37 +0200

    #3 SETUP [Service connexion] WIP.

--- src/ClientMarionServiceWeb/ClientMessagerie.java
+++ src/ClientMarionServiceWeb/ClientMessagerie.java
@@ -6,7 +6,6 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
-
 import model.User;
 
 import com.sun.jersey.api.client.Client;
@@ -34,12 +33,40 @@ public class ClientMessagerie {
 //		context = JAXBContext.newInstance(User.class); 
 //		unmarshaller = context.createUnmarshaller();
 		
-		reponse = serviceMessagerie.path("inscription/" + "Marion" + "/" + "password").get(String.class);    
+		reponse = serviceMessagerie.path("inscription/marion/password").get(String.class);    
 
 		/*
 		 ** Création du user (normalement récupéré par Scan)
 		 */
 		System.out.println(reponse);
+	}
+	
+	private static void connexion() throws Exception {
+
+		String reponse;
+		StringBuffer xmlStr;
+		JAXBContext context;       
+		JAXBElement<User> root;       
+		Unmarshaller unmarshaller;
+		System.out.println("toto");
+		
+		//PLANTE ICI ! 
+		reponse = serviceMessagerie.path("connexion/marion/password").get(String.class);
+		System.out.println(reponse);
+		/*
+		 ** Instanciation du convertiseur XML => Objet Java
+		 */
+		context = JAXBContext.newInstance(User.class); 
+		unmarshaller = context.createUnmarshaller();
+		
+		xmlStr = new StringBuffer(reponse);
+		root = unmarshaller.unmarshal(new StreamSource(new StringReader(xmlStr.toString())), User.class);
+		System.out.println("toto");
+		System.out.println(root.getValue().getLogin());
+		System.out.println(root.getValue().getPassword());
+		
+
+		
 	}
 
 	public static void main(String args[]) throws Exception 
@@ -47,6 +74,6 @@ public class ClientMessagerie {
 		User new_user;
 		serviceMessagerie = Client.create().resource("http://localhost:8080/Messagerie");
 
-		submit_subscription();
+		connexion();
 	}    
 }
--- src/messagerie/Tier2.java
+++ src/messagerie/Tier2.java
@@ -3,14 +3,17 @@ package messagerie;
 import java.rmi.Naming;
 import java.util.HashMap;
 
+import javax.annotation.Resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.JAXBElement;
+import javax.xml.ws.WebServiceContext;
 
 import model.User;
 
@@ -20,14 +23,13 @@ public class Tier2 {
 
 	//private static final HashMap<String,Integer> temperatures;
 
-	//private static User current_user;
+	private static User current_user;
 	//private static Users users;
 
 	@GET
-	@Path("inscription/{login}")
+	@Path("inscription/{login}/{password}")
 	@Produces("text/plain")
-	//TODOOOOO :: Passer plusieurs paramètres.
-	public String inscription (@PathParam("login") String login)
+	public String inscription (@PathParam("login") String login, @PathParam("password") String password)
 	{
 		// Vérification des données en base
         // Appel RMI
@@ -37,13 +39,48 @@ public class Tier2 {
 		} catch (Exception e){
 
 		}
-//         tier_3.search_user(login);
-//         new_user = tier_3.add_new_user(login, password);
-//         if(new_user){
-//        	return true;
-//         }
-		 User new_user = new User(login,"pssword");
-         return new_user.getLogin();
+//         User user = tier_3.search_user(login);
+//		if(user){
+//			return "Un utilisateur existe déjà avec ce login";
+//		} else {
+//			new_user = tier_3.add_new_user(login, password);
+//	        if(new_user){
+//	      	 	return "Votre inscription a bien été prise en compte";
+//	        } else {
+//				return "Une erreur est survenue lors de l'inscription";
+//		    }
+//		}
+		
+		return "Toto";
+	}
+	
+	@GET
+	//@Resource WebServiceContext wsContext;
+	@Path("connexion/{login}/{password}")
+	@Produces("text/plain")
+	public User connexion (@PathParam("login") String login, @PathParam("password") String password)
+	{
+		// Vérification des données en base
+        // Appel RMI
+		// localhost chez nous sinon ip du serveur. Messagerie = Interface de la bdd
+		try {
+	         Tier3 tier_3 = (Tier3) Naming.lookup("rmi://localhost:2000/Messagerie");
+		} catch (Exception e){
+
+		}
+//         User current_user = tier_3.search_user(login);
+//		if(!current_user){
+//			return "Cet utilisateur n'existe pas";
+//		} else {
+//	        if(current_user.getPassword().equals(password)){
+//	      	 	return "Connecté avec succès";
+//	        } else {
+//				return "Connexion impossible";
+//		    }
+//		}
+
+		 current_user = new User(login,password);
+         return current_user;
 	}
 
 	@GET
