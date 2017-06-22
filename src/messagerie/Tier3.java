d38d2af0c50b2bc3d4b0993a79a07bfe93e6ab64
ElodieGoy <elodie.goy@gmail.com>
2017-06-22 08:33:16 +0200

    #1 UPDTAE Tier3 AND Tier3impl

--- src/messagerie/Tier3.java
+++ src/messagerie/Tier3.java
@@ -1,22 +1,16 @@
 package messagerie;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-import model.ServiceInscriptionImpl;
 
-//import model.ServiceInscriptionImpl;
+public interface Tier3 extends Remote{
 
-public class Tier3 {
-
-	public static void main (String arg[]) throws Exception{
-		
-		System.setProperty("java.rmi.server.hostname", "127.0.0.1");
-		
-		Registry registry = LocateRegistry.createRegistry(2000);
-		
-		ServiceInscriptionImpl serviceInscription = new ServiceInscriptionImpl();
-		
-		registry.bind("ServiceInscription", serviceInscription);
-	}
+	public void find_user(String login) throws RemoteException;
+	
+	public void inscrire() throws RemoteException;
+	
+	
 }
